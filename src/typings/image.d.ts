95a7ae4a0379554fa5a910285f86f33a515ccfce
Kerman Kohli <kermankohli@gmail.com>
2019-03-19 20:17:24 +1100

    Cleanup

--- src/typings/image.d.ts
+++ src/typings/image.d.ts
@@ -1,4 +1,9 @@
-declare module '*.png';
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
 
 declare module '*.svg' {
   const content: any;
